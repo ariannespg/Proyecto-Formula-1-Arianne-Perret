12874b6bc553f6c968fb3949ba3924f7809b1a64
ariannespg <113394844+ariannespg@users.noreply.github.com>
2023-03-29 23:25:14 -0400

    Delete prueba.py

--- prueba.py
+++
@@ -1,8 +0,0 @@
-cedula_cliente = input("\nIntroduce tu numero de cedula\n ->")
-while not cedula_cliente.isnumeric():
-    cedula_cliente = input("\nIntroduce tu numero de cedula con solo caracteres numericos\n ->")
-
-print (cedula_cliente)
-
-
-
